60051023a8ab835f7f79086d857ae4cb16d8a04e
therajesh1 <therajeshchheda@gmail.com>
2024-10-30 12:50:19 +0530

    asx

--- furnicure/settings.py
+++ furnicure/settings.py
@@ -142,8 +142,7 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 import os
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build','static')
-STATICFILES_DIR = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Add this if you have media files
 MEDIA_URL = '/media/'
